ef8ee6670c43ed3e75bfb124841179581730e32f
souared <sofiane.kouared@gmail.com>
2022-04-14 11:24:42 -0600

    Initial Load

---
+++ src/app/shared/models/category.model.ts
@@ -0,0 +1,15 @@
+export interface CategoryModel {
+  categoryID: string;
+  categoryName: string;
+  categoryDescription: string;
+  createdBy: string;
+  createdOn: Date;
+  updatedBy: string;
+  updatedOn: Date;
+  user: string;
+  active: boolean;
+
+
+}
+
+export type CategoryRequiredProps = Pick<CategoryModel, 'categoryName'>;
