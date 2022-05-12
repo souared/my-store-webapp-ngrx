8198bb00bd8f6fbd089652f40735ef09244b954c
souared <sofiane.kouared@gmail.com>
2022-05-12 09:53:30 -0600

    Order total Calculation

--- src/app/order/components/order-details/order-details.component.ts
+++ src/app/order/components/order-details/order-details.component.ts
@@ -9,14 +9,17 @@ import { OrdersPageActions } from '../../actions';
 @Component({
   selector: 'app-order-details',
   templateUrl: './order-details.component.html',
-  styleUrls: ['./order-details.component.scss']
+  styleUrls: ['./order-details.component.scss'],
 })
 export class OrderDetailsComponent implements OnInit {
   orderId!: string;
   selectedOrder$: Observable<OrderModel | null>;
   ordr!: OrderModel;
+  totalOrderPrice: number = 0;
+  totalGst: number = 0;
+  subtotal: number = 0;
 
-  constructor(private store: Store<State>,private route: ActivatedRoute) {
+  constructor(private store: Store<State>, private route: ActivatedRoute) {
     this.selectedOrder$ = store.select(selectActiveOrder);
   }
 
@@ -25,15 +28,28 @@ export class OrderDetailsComponent implements OnInit {
       this.orderId = params['id'];
     });
 
-    this.store.dispatch(OrdersPageActions.LoadSingleOrder({orderId: this.orderId}));
+    this.store.dispatch(
+      OrdersPageActions.LoadSingleOrder({ orderId: this.orderId })
+    );
     // this.store.dispatch(
     //   OrdersPageActions.selectOrder({orderId: this.orderId })
     // );
     this.selectedOrder$.subscribe((x) => {
       if (x !== null) {
         this.ordr = x;
+
+        if (this.ordr.orderDetails?.length !== 0) {
+          this.ordr.orderDetails?.forEach((x) => {
+            this.totalOrderPrice += x.qty * x.product.listingPrice;
+            this.totalGst += x.qty * x.product.listingPrice * 0.05;
+            this.subtotal = this.totalOrderPrice + this.totalGst;
+          });
+        }
       }
     });
-  }
 
+    // this.totalOrderPrice = 0;
+    // this.totalGst = 0;
+    // this.subtotal = 0;
+  }
 }
